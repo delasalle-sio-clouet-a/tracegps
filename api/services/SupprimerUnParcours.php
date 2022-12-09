afc23a9c65bb7bd4aff616bb2e3b5b81196b08c0
wandev35 <e.forveille.mail@gmail.com>
2022-12-09 10:00:05 +0100

    Fin du développement du service SupprimerUnParcours

--- api/services/SupprimerUnParcours.php
+++ api/services/SupprimerUnParcours.php
@@ -56,25 +56,95 @@ else
                 $msg = "Erreur : vous n'êtes pas le propriétaire de ce parcours."; 
                 $code_reponse = 400; 
             }
+            else 
+            {
+               // Si tout est bon, on peut effectuer la supression
+               $ok = $dao ->supprimerUneTrace($idTrace);
+               if ( ! $ok )
+               {
+                   $msg = "Erreur : problème lors de la suppression du parcours.";
+                   $code_reponse = 500;
+               }
+               else {
+                   $msg = "Parcours supprimé.";
+                   $code_reponse = 200;
+               }
+            }
         }
     }
 }
+unset($dao);   // ferme la connexion à MySQL
 
+//création du flux en sortie
+if($lang=="xml") {
+    $content_type = "application/xml; charset=utf-8"; //indique le format XML pour la réponse
+    $donnees = creerFluxXML($msg);
+}
+else {
+    $content_type = "application/json; charset=utf-8"; //indique le format JSon pour la réponse
+    $donnees = creerFluxJSON($msg);
+    
+}
 
+//envoi de la réponse HTTP
+$this->envoyerReponse($code_reponse, $content_type, $donnees);
 
+//fin du programme pour ne pas enchainer sur les 2 fonctions qui suivent
+exit;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?> 
+// =======================================================================================================================
+// création du flux XML en sortie
+function creerFluxXML($msg)
+{	// crée une instance de DOMdocument (DOM : Document Object Model)
+    $doc = new DOMDocument();
+    
+    // specifie la version et le type d'encodage
+    $doc->version = '1.0';
+    $doc->encoding = 'UTF-8';
+    
+    // crée un commentaire et l'encode en UTF-8
+    $elt_commentaire = $doc->createComment('Service web SupprimerUnParcours - BTS SIO - Lycée De La Salle - Rennes');
+    // place ce commentaire à la racine du document XML
+    $doc->appendChild($elt_commentaire);
+    
+    // crée l'élément 'data' à la racine du document XML
+    $elt_data = $doc->createElement('data');
+    $doc->appendChild($elt_data);
+    
+    // place l'élément 'reponse' dans l'élément 'data'
+    $elt_reponse = $doc->createElement('reponse', $msg);
+    $elt_data->appendChild($elt_reponse);
+    
+    // Mise en forme finale
+    $doc->formatOutput = true;
+    
+    // renvoie le contenu XML
+    return $doc->saveXML();
+}
+// ================================================================================================
+// création du flux JSON en sortie
+function creerFluxJSON($msg)
+{
+    /* Exemple de code JSON
+     {
+     "data":{
+     "reponse": "authentification incorrecte."
+     }
+     }
+     */
+    
+    // 2 notations possibles pour créer des tableaux associatifs (la deuxième est en commentaire)
+    
+    // construction de l'élément "data"
+    $elt_data = ["reponse" => $msg];
+    //     $elt_data = array("reponse" => $msg);
+    
+    // construction de la racine
+    $elt_racine = ["data" => $elt_data];
+    //     $elt_racine = array("data" => $elt_data);
+    
+    // retourne le contenu JSON (l'option JSON_PRETTY_PRINT gère les sauts de ligne et l'indentation)
+    return json_encode($elt_racine, JSON_PRETTY_PRINT);
+}
+// ================================================================================================
+?>
